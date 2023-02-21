68e1dcd0aa09cd429f2f8ccfc4d8431efe553d9b
Your Name <deckerwill7@gmail.com>
2023-02-20 21:12:02 -0600

    mean-smooth filter

--- time_series_denoising.m
+++ time_series_denoising.m
@@ -1,4 +1,5 @@
 %% Time Series Denoising
+% Author: Will Decker
 % Code adapted from Mike X. Cohen's "Signal Processing Problems" course
 
 %% Section 1: Mean-smooth time series
@@ -21,10 +22,51 @@ ampl = interp1(randn(p,1)*25, linspace(1,p,n));
 
 % generate signal
 
-figure(1), clf
 signal = ampl + noise;
-subplot(221)
-plot(time, ampl), title('signal without noise')
-subplot(222)
-plot(signal) , title('signal with added noise')
 
+figure(1), clf, hold on
+subplot(221)
+plot(time, ampl, 'Color', [0.6, 0 ,0]), title('raw signal without noise')
+subplot(222)
+plot(signal) , title('raw signal with added noise')
+
+% show signals superimposed on eachother 
+
+figure(2), clf, hold on
+plot(time, signal, time, ampl, 'linew', 2)
+zoom on
+
+% filtered signal vector
+
+filtsig = ones(size(signal)); % or filtsig = signal
+
+% implement the mean-smooth in the *time* domain
+
+k = 100; % filter window
+
+for i = k + 1:n - k - 1
+
+    filtsig(i) = mean(signal(i - k:i + k)); % each point along the filtered signal is the average of 'k' surrounding points
+
+end
+
+% convert windosize to ms
+
+windowsize = 1000 * (k*2 + 1) / samplerate;
+
+% plot noisy and filtered signal
+
+figure(3), clf, hold on
+plot(time, signal, time, filtsig, 'linew', 2)
+timeindex = dsearchn(time', 1);
+ylim = get(gca, 'ylim');
+patch(time([ timeindex-k timeindex-k timeindex+k timeindex+k ]),ylim([ 1 2 2 1 ]),'k','facealpha',.25,'linestyle','none')
+plot(time([timeindex timeindex]),ylim,'k--')
+xlabel('Time (sec.)'), ylabel('Amplitude')
+title([ 'Running-mean filter with a k=' num2str(round(windowsize)) '-ms filter' ])
+legend({'Signal';'Filtered';'Window';'window center'})
+
+zoom on 
+
+
+%%
